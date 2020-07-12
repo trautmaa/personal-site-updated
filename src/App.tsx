5f37e2ca6cc067b93410960771f58100a7eafdfa
Alex Trautman <alexander.trautman@honeywell.com>
2020-07-12 10:29:00 -0400

    remove window reference from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import { Root, Routes, addPrefetchExcludes } from 'react-static';
+import { Root, Routes } from 'react-static';
 import { Router } from '@reach/router';
 import FancyDiv from 'components/FancyDiv';
 import Dynamic from 'containers/Dynamic';
 import './app.css';
 import './styles/main.scss';
 
-// Any routes that start with 'dynamic' will be treated as non-static routes
-addPrefetchExcludes(['dynamic']);
-
 function App() {
-  console.log('App -> window.location.pathname', window.location.pathname);
   return (
     <Root>
-      {/* <nav> */}
-        {/* <Link className='home' to='/'> */}
-          {/* Alex Trautman */}
-        {/* </Link> */}
-        {/* <Link to='/blog'>Blog</Link> */}
-        {/* <Link to='/dynamic'>Dynamic</Link> */}
-      {/* </nav> */}
       <div className='content'>
         <FancyDiv>
           <React.Suspense fallback={<em>Loading...</em>}>
